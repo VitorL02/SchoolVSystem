573983006695a395c6e395ab50f2172723eb7fc9
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-10-14 16:43:01 -0300

    iniciando projeto

---
+++ src/main/java/com/schoolv/schoolvsystem/SchoolvsystemApplication.java
@@ -0,0 +1,13 @@
+package com.schoolv.schoolvsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolvsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolvsystemApplication.class, args);
+	}
+
+}
