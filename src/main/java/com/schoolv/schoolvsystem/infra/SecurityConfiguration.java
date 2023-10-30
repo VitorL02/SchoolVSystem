a443db8d146fa843d3512065f5b92669d5d8da38
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-10-30 15:48:35 -0300

    Adicionando o swagger e documentando os usuarios

--- src/main/java/com/schoolv/schoolvsystem/infra/SecurityConfiguration.java
+++ src/main/java/com/schoolv/schoolvsystem/infra/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.schoolv.schoolvsystem.infra;
 
 
 import com.schoolv.schoolvsystem.filter.SecurityFilter;
+import io.swagger.v3.oas.models.PathItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,8 +29,14 @@ public class SecurityConfiguration {
             http.csrf().disable()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         .and()
-                    .authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.POST, "/users/login").permitAll().requestMatchers(HttpMethod.POST, "/users/register").permitAll()
-                                .requestMatchers(HttpMethod.POST,"/users/register/staff").hasAnyRole("ADMIN").requestMatchers(HttpMethod.POST,"/users/register/professor").hasAnyRole("STAFF","ADMIN").anyRequest().authenticated()
+                    .authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.POST, "/users/login").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/swagger-resources/**").permitAll()
+                            .requestMatchers("/swagger-ui.html").permitAll()
+                            .requestMatchers("/v3/**").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/users/register/staff").hasAnyRole("ADMIN")
+                            .requestMatchers(HttpMethod.POST,"/users/register/professor").hasAnyRole("STAFF","ADMIN").anyRequest().authenticated()
                     )
                     .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
